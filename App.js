7d737aaca0f434bd14d46691c415865fa509385a
MikePodo <72575260+MikePodo@users.noreply.github.com>
2020-12-19 10:56:05 -0800

    Card component

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <Card
+      title="Title"
+      subTitle="Subtitle"
+      image={require("./app/assets/jacket.jpg")}
+    />
+  );
+  // return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
