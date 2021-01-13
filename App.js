0209575775ee4c8f83d485da907b22429a7eab29
MikePodo <72575260+MikePodo@users.noreply.github.com>
2021-01-13 10:46:14 -0800

    offline caching

--- App.js
+++ App.js
@@ -2,16 +2,15 @@ import React from "react";
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
-import Screen from "./app/components/Screen";
-import AuthNavigator from "./app/navigation/AuthNavigator";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
     </NavigationContainer>
   );
-}
+};
+
+export default App;
