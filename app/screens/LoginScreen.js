cd2ec5852464edc4b26c7cf27e127ee935fe8734
MikePodo <72575260+MikePodo@users.noreply.github.com>
2021-01-15 22:30:58 -0800

    refactor code with custom useAuth hook

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,7 +1,6 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Image, StyleSheet } from "react-native";
 import * as Yup from "yup";
-import jwtDecode from "jwt-decode";
 
 import Screen from "../components/Screen";
 import {
@@ -11,8 +10,7 @@ import {
   SubmitButton,
 } from "../components/forms";
 import authApi from "../api/auth";
-import AuthContext from "../auth/context";
-import authStorage from "../auth/storage";
+import useAuth from "../auth/useAuth";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -20,7 +18,7 @@ const validationSchema = Yup.object().shape({
 });
 
 export default function LoginScreen() {
-  const authContext = useContext(AuthContext);
+  const { logIn } = useAuth();
 
   const [loginFailed, setLoginFailed] = useState(false);
 
@@ -28,9 +26,7 @@ export default function LoginScreen() {
     const result = await authApi.login(email, password);
     if (!result.ok) return setLoginFailed(true);
     setLoginFailed(false);
-    const user = jwtDecode(result.data);
-    authContext.setUser(user);
-    authStorage.storeToken(result.data);
+    logIn(result.data);
   };
 
   return (
