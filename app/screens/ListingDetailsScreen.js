fe81e280edb60d94be19d1bed411f0c77e49abf4
MikePodo <72575260+MikePodo@users.noreply.github.com>
2021-01-17 11:32:14 -0800

    add contact form to listing details screen

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,36 +1,42 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
 import { Image } from "react-native-expo-image-cache";
 
 import AppText from "../components/AppText";
 import colors from "../config/colors";
 import ListItem from "../components/lists/ListItem";
+import ContactSellerForm from "../components/ContactSellerForm";
 
 export default function ListingDetailsScreen({ route }) {
   const listing = route.params;
-  console.log(listing);
 
   return (
-    <View>
-      <Image
-        style={styles.image}
-        preview={{ uri: listing.images[0].thumbnailUrl }}
-        tint="light"
-        uri={listing.images[0].url}
-      />
+    <KeyboardAvoidingView
+      behavior="position"
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 100}
+    >
+      <View>
+        <Image
+          style={styles.image}
+          preview={{ uri: listing.images[0].thumbnailUrl }}
+          tint="light"
+          uri={listing.images[0].url}
+        />
 
-      <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{listing.title}</AppText>
-        <AppText style={styles.price}>${listing.price}</AppText>
-        <View style={styles.userContainer}>
-          <ListItem
-            image={require("../assets/guitar-logo.jpg")}
-            title="Michael Podolsky"
-            subTitle="5 Listings"
-          />
+        <View style={styles.detailsContainer}>
+          <AppText style={styles.title}>{listing.title}</AppText>
+          <AppText style={styles.price}>${listing.price}</AppText>
+          <View style={styles.userContainer}>
+            <ListItem
+              image={require("../assets/guitar-logo.jpg")}
+              title="Michael Podolsky"
+              subTitle="5 Listings"
+            />
+          </View>
+          <ContactSellerForm listing={listing} />
         </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
